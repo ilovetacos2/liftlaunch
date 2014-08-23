6b76881a48a8e2131a48e22cf16d76e690da4d35
enkhalifapro <enkhalifapro@gmail.com>
2014-08-23 04:29:53 -0700

    update conf

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,6 @@ module Prelaunchr
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
     #config.assets.initialize_on_precompile = false
-    config.assets.compile = true
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
